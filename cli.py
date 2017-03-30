099322b0fa0c296c5b4cecf33196f0fe1175b83a
Adam Glass <adamglass@spottedfoobar.com>
2017-03-30 00:51:34 -0700

    coffee along route

--- cli.py
+++ cli.py
@@ -1,7 +1,11 @@
 from gpx import GpxParser
 from filters import regular_intervals
+from places import place_search, place_detail
 
 with open('SIR_Permanent_1845__Flat_Snohomish-Edison_200K.gpx') as f:
     parser = GpxParser(f)
     for p in regular_intervals(parser.points_with_attributes(), 10.0):
         print(p)
+        for place in place_search(p['lat'], p['lon'], 'coffee'):
+            detail = place_detail(place[1])
+            print('\t{0}'.format(detail['name']))
