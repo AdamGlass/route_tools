6965d3b41a42ef6b2af690284d37ca0c4d6882d6
Adam Glass <adamglass@spottedfoobar.com>
2017-04-24 21:45:23 -0700

    weather data gather

--- route_tool.py
+++ route_tool.py
@@ -1,13 +1,16 @@
 import argparse
+import json
 
 import requests
 
 from gpx import GpxParser
 from places import place_route_data
+from weather import weather_route_data
 
 parser = argparse.ArgumentParser(description='route tool')
 parser.add_argument('route', type=str)
 parser.add_argument('--places', action='store_true')
+parser.add_argument('--weather', action='store_true')
 parser.add_argument('-interval', type=int, default=5)
 
 args = parser.parse_args()
@@ -21,11 +24,12 @@ else:
 
 gpx = GpxParser(gpxdata)
 
-print (args)
-
 if args.places:
     data = place_route_data(gpx)
-    print(data)
 
     for d in data:
         print('{0}, {1}, {2}'.format(d.name, d.address, d.opening_hours_text))
+
+if args.weather:
+    data = weather_route_data(gpx)
+    print(data)
---
+++ weather.py
@@ -0,0 +1,41 @@
+import os
+from collections import namedtuple
+
+import requests
+
+from filters import regular_intervals
+
+forecast_url = 'https://api.darksky.net/forecast/{0}/{1},{2}'
+
+ForecastData = namedtuple('forecastdata', 'time temperature tempMin, tempMax, pressure chancePrecip precipIntensity windBearing windSpeed')
+
+def forecast_request(key, lat, lng):
+    r = requests.get(forecast_url.format(key,lat,lng))
+    return r.json()
+
+def forecast_decode(forecast):
+    return ForecastData(forecast.get('time'),
+                        forecast.get('temperature'),
+                        forecast.get('temperatureMin'),
+                        forecast.get('temperatureMax'),
+                        forecast.get('pressure'),
+                        forecast.get('precipProbability'),
+                        forecast.get('precipIntensity'),
+                        forecast.get('windBearing'),
+                        forecast.get('windSpeed'))
+
+def forecast_to_sequence(forecast):
+    yield forecast_decode(forecast['currently'])
+    for h in forecast['hourly']['data']:
+        yield forecast_decode(h)
+    for d in forecast['daily']['data']:
+        yield forecast_decode(d)
+
+def weather_route_data (parser):
+    weather = []
+    key = os.environ['FORECAST_API_KEY']
+
+    for p in regular_intervals(parser.points_with_attributes(), 10):
+        forecast = forecast_request(key, p['lat'], p['lon'])
+        weather.append(list(forecast_to_sequence(forecast)))
+    return weather
