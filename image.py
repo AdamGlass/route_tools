70f66e78be6f603efec72faf0f1aa20b21c34b39
Adam Glass <adamglass@spottedfoobar.com>
2019-03-10 18:32:42 -0700

    address url length issues by increasing interval until it fits

--- image.py
+++ image.py
@@ -12,9 +12,8 @@ mapbox_url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/{1}/aut
 
 def get_static_map(image, overlays):
 
-    quoted_overlays = ','.join([quote(o) for o in overlays])
+    quoted_overlays = ','.join(overlays)
     url = mapbox_url.format(os.environ['MAPBOX_TOKEN'], quoted_overlays)
-    print(len(url))
     r = requests.get(url)
     if r.status_code == 200:
         with open(image, 'wb') as f:
@@ -25,15 +24,25 @@ def get_static_map(image, overlays):
 
 
 def image_route_compare(intended_gpx, ridden_gpx, image):
-    intended_points = [(p['lat'],p['lon']) for p in regular_intervals(intended_gpx.points_with_attributes(), 1)]
-    ridden_points = [(p['lat'],p['lon']) for p in regular_intervals(ridden_gpx.points_with_attributes(), 1)]
 
-    intended_polyline = polyline.encode(intended_points)
-    ridden_polyline = polyline.encode(ridden_points)
-    start = 'pin-l-bicycle+00ff00({0},{1})'.format(ridden_points[0][1], ridden_points[0][0])
-    end = 'pin-s-bicycle+ff0000({0},{1})'.format(ridden_points[0][1], ridden_points[0][0])
-    intended = 'path-3+ff0000-0.5({0})'.format(intended_polyline)
-    ridden = 'path-3+0000ff-0.5({0})'.format(ridden_polyline)
+    interval = 1
+    while True:
+        intended_points = [(p['lat'],p['lon']) for p in regular_intervals(intended_gpx.points_with_attributes(), interval)]
+        ridden_points = [(p['lat'],p['lon']) for p in regular_intervals(ridden_gpx.points_with_attributes(), interval)]
+        intended_polyline = polyline.encode(intended_points)
+        ridden_polyline = polyline.encode(ridden_points)
+
+        intended = quote('path-3+ff0000-0.5({0})'.format(intended_polyline))
+        ridden = quote('path-3+0000ff-0.5({0})'.format(ridden_polyline))
+        if len(intended) + len(ridden) > 7000:
+            interval += 1
+            continue
+        else:
+            break
+
+    print(interval)
+    start = quote('pin-l-bicycle+00ff00({0},{1})'.format(ridden_points[0][1], ridden_points[0][0]))
+    end = quote('pin-s-bicycle+ff0000({0},{1})'.format(ridden_points[0][1], ridden_points[0][0]))
     get_static_map(image, [start, end, intended, ridden])
 
 def image_route(gpx, image):
@@ -41,7 +50,7 @@ def image_route(gpx, image):
     points = [(p['lat'],p['lon']) for p in regular_intervals(gpx.points_with_attributes(), 1)]
 
     encoded_polyline = polyline.encode(points)
-    start = 'pin-l-bicycle+00ff00({0},{1})'.format(points[0][1], points[0][0])
-    end = 'pin-s-bicycle+ff0000({0},{1})'.format(points[0][1], points[0][0])
-    path = 'path-3+0000ff-0.5({0})'.format(encoded_polyline)
+    start = quote('pin-l-bicycle+00ff00({0},{1})'.format(points[0][1], points[0][0]))
+    end = quote('pin-s-bicycle+ff0000({0},{1})'.format(points[0][1], points[0][0]))
+    path = quote('path-3+0000ff-0.5({0})'.format(encoded_polyline))
     get_static_map(image, [start, end, path])
